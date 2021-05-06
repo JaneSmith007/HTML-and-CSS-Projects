a7d385396649957789a939c1451732cc9e5a5c6a
LauraSmith007 <lauravsmith007@gmail.com>
2021-05-06 15:59:04 -0700

    created new folder Basic_HTML_3 and deleted some duplicate files

--- Basic_JavaScript_1.js
+++
@@ -1,9 +0,0 @@
-
-                function My_First_Function() {
-                var String = "Kiss Me I'm Irish!";
-                var result = String.fontcolor("green");
-                document.getElementById("Irish").innerHTML =
-                result;
-            }
-
-       
